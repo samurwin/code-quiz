0e4106810caa73830d65caf2f0452ec743bfac6d
smudesign <urwinsamantha@gmail.com>
2021-09-20 21:12:47 -0400

    quiz questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var startPageEl = document.querySelector("#start");
 var startButtonEl = document.querySelector("#btn-start");
 var pageContentEl = document.querySelector("#page-content");
 var timerEl = document.querySelector("#timer");
+var flexContainerEl = document.querySelector("#container");
 
 var questionOne = {
     question: "Commonly used data types DO NOT include:",
@@ -45,62 +46,63 @@ var questionFive = {
 };
 
 var questions = [questionOne, questionTwo, questionThree, questionFour, questionFive];
+var currentQuestion = 0;
 
-var createQuestion = function() {
+var createQuestion = function(questions) {
     // remove startPageEl
     startPageEl.remove();
 
-    // create flexbox container for the question
-    var flexContainer = document.createElement("div");
-    flexContainer.className = "question-container";
-
     // create <div> with the class of .question
-    var questionContainer = document.createElement("div");
-    questionContainer.className = "question";
+    var questionContainerEl = document.createElement("div");
+    questionContainerEl.className = "question";
     // append the div to the flex container
-    flexContainer.appendChild(questionContainer);
+    flexContainerEl.appendChild(questionContainerEl);
 
     // create <h1> with text content of questions[i].question
-    var askQuestion = document.createElement("h1");
-    askQuestion.textContent= questions[0].question;
+    var askQuestionEl = document.createElement("h1");
+    askQuestionEl.textContent= questions.question;
     // append to div
-    questionContainer.appendChild(askQuestion);
+    questionContainerEl.appendChild(askQuestionEl);
 
     // create <a> with the text content of questions[i].1 and an id="1"
-    var answerOne = document.createElement("a");
-    answerOne.textContent = questions[0].one;
-    answerOne.setAttribute("id", "1");
+    var answerOneEl = document.createElement("a");
+    answerOneEl.textContent = questions.one;
+    answerOneEl.setAttribute("id", "1");
+    answerOneEl.className = "answer";
     // apend to div
-    questionContainer.appendChild(answerOne);
+    questionContainerEl.appendChild(answerOneEl);
 
     // create <a> with the text content of questions[i].2 and an id="2"
-    var answerTwo = document.createElement("a");
-    answerTwo.textContent = questions[0].two;
-    answerTwo.setAttribute("id", "2");
+    var answerTwoEl = document.createElement("a");
+    answerTwoEl.textContent = questions.two;
+    answerTwoEl.setAttribute("id", "2");
+    answerTwoEl.className = "answer";
     // append to div
-    questionContainer.appendChild(answerTwo);
+    questionContainerEl.appendChild(answerTwoEl);
 
     // create <a> with the text content of questions[i].3 and an id="3"
-    var answerThree = document.createElement("a");
-    answerThree.textContent = questions[0].three;
-    answerThree.setAttribute("id", "3");
+    var answerThreeEl = document.createElement("a");
+    answerThreeEl.textContent = questions.three;
+    answerThreeEl.setAttribute("id", "3");
+    answerThreeEl.className = "answer";
     // append to div
-    questionContainer.appendChild(answerThree);
+    questionContainerEl.appendChild(answerThreeEl);
 
     // create <a> with the text content of questions[i].4 and an id="4"
-    var answerFour = document.createElement("a");
-    answerFour.textContent = questions[0].four;
-    answerFour.setAttribute("id", "4");
+    var answerFourEl = document.createElement("a");
+    answerFourEl.textContent = questions.four;
+    answerFourEl.setAttribute("id", "4");
+    answerFourEl.className = "answer";
     //append to div
-    questionContainer.appendChild(answerFour);
+    questionContainerEl.appendChild(answerFourEl);
 
     // append div to the body
-    pageContentEl.appendChild(flexContainer);
+    pageContentEl.appendChild(flexContainerEl);
+
 }
 
 timer = function() {
-    var time = 75;
-    timerEl.textContent = time;
+    var time = timerEl.textContent;
     var timeLeft = setInterval(function() {
         if (time > 0){
             timerEl.textContent = time;
@@ -112,9 +114,99 @@ timer = function() {
 }
 
 var startQuiz = function(event) {
-    createQuestion();
+    timerEl.textContent = 75;
     timer();
-}
-startButtonEl.addEventListener("click", startQuiz);
 
-// createQuestion();
+    createQuestion(questions[currentQuestion]);
+}
+
+var answerHandler = function(event) {
+    var targetEl = event.target
+    if (targetEl.matches(".answer")) {
+        checkAnswer(targetEl);
+    };
+}
+
+var checkAnswer = function(answerSubmitted) {
+    if (answerSubmitted.id === questions[currentQuestion].correct) {
+        console.log("Correct");
+        var correctEl = document.createElement("h2");
+        correctEl.className = "answer-status";
+        correctEl.textContent = "Correct!";
+        flexContainerEl.appendChild(correctEl);
+        setTimeout(function() {
+            correctEl.remove()
+        }, 3000);
+        nextQuestion();
+    } else {
+        console.log("Incorrect");
+        var incorrectEl = document.createElement("h2");
+        incorrectEl.className = "answer-status";
+        incorrectEl.textContent = "Wrong!";
+        flexContainerEl.appendChild(incorrectEl);
+        setTimeout( function() {
+            incorrectEl.remove()
+        }, 3000);
+        wrongAnswer();
+        nextQuestion();
+    }
+}
+
+var wrongAnswer = function() {
+    var newTime = timerEl.textContent;
+    newTime = newTime - 10;
+    timerEl.textContent = newTime;
+}
+
+var nextQuestion = function() {
+    currentQuestion = currentQuestion + 1;
+    console.log(currentQuestion);
+    if (currentQuestion > 4) {
+        endGame();
+    } else {
+        document.querySelector(".question").remove();
+        createQuestion(questions[currentQuestion]);
+    }
+}
+
+var endGame = function() {
+    document.querySelector(".question").remove();
+
+    var endContainerEl = document.createElement("div");
+    endContainerEl.className = "question";
+
+    var doneEl = document.createElement("h1");
+    doneEl.textContent = "All Done!";
+    endContainerEl.appendChild(doneEl);
+
+    var finalScoreEl = document.createElement("h2");
+    finalScoreEl.textContent = "Your final score is " + timerEl.textContent + " .";
+    endContainerEl.appendChild(finalScoreEl);
+
+    var formContainerEl = document.createElement("div");
+    formContainerEl.className = "form-container";
+    endContainerEl.appendChild(formContainerEl);
+    
+    var inputTitleEl = document.createElement("h2");
+    inputTitleEl.textContent = "Enter your initials:";
+    formContainerEl.appendChild(inputTitleEl);
+
+    var inputFieldEl = document.createElement("input");
+    inputFieldEl.className = "inputField";
+    formContainerEl.appendChild(inputFieldEl);
+
+    var submitButtonEl = document.createElement("button");
+    submitButtonEl.setAttribute("id", "submit-score");
+    submitButtonEl.textContent = "Submit";
+    formContainerEl.appendChild(submitButtonEl);
+
+    flexContainerEl.appendChild(endContainerEl);
+}
+
+var submitScore = function() {
+    
+}
+
+flexContainerEl.addEventListener("click", submitScore);
+startButtonEl.addEventListener("click", startQuiz);
+pageContentEl.addEventListener("click", answerHandler);
